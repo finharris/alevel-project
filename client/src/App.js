ee1c108bc1e14b766d318f8b84d7f9e47bd0881f
finharris <mybutiskfc@gmail.com>
2022-06-13 16:47:56 +0100

    version 1

--- client/src/App.js
+++ client/src/App.js
@@ -16,9 +16,43 @@ function App() {
     getProducts();
   }
 
+  const idRef = createRef();
+
   return (
     <>
       <MainLayout></MainLayout>
+      <input type='button' value='get products' onClick={() => getProducts()} />
+
+      <br />
+      <input
+        type='button'
+        value='remove product'
+        onClick={() => removeProduct(idRef.current.value)}
+      />
+      <input type='text' name='id' id='id' ref={idRef} />
+
+      <table>
+        <thead>
+          <tr>
+            <td>id</td>
+            <td>name</td>
+            <td>cat id</td>
+            <td>buy cost</td>
+            <td>sell cost</td>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product) => (
+            <tr>
+              <td>{product.id}</td>
+              <td>{product.name}</td>
+              <td>{product.category_id}</td>
+              <td>{product.purchase_cost}</td>
+              <td>{product.selling_cost}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 }

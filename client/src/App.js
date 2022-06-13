22f473043e2f9028fd04aa31f8cd63df71129638
finharris <mybutiskfc@gmail.com>
2022-06-13 09:09:50 +0100

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,68 @@
-import React, { useState } from "react";
-
-async function getFirstName(name) {
-  const data = await fetch(`/api/users?firstname=${name}`)
-    .then((res) => res.json())
-    .then((data) => console.log(data));
-}
+import React, { useEffect, useState, createRef } from "react";
+import "./App.css";
 
 function App() {
+  const [returnData, setReturnData] = useState([]);
+
+  const textBoxRef = createRef();
+
+  async function getFirstName() {
+    let res;
+    if (!textBoxRef.current.value) {
+      res = await fetch(`/api/users`);
+    } else {
+      res = await fetch(`/api/users?firstname=${textBoxRef.current.value}`);
+    }
+    const data = await res.json();
+    if (data.length === 0) {
+      setReturnData(null);
+    } else {
+      setReturnData(data);
+    }
+  }
+
+  useEffect(() => {
+    getFirstName();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <>
-      <h1>test</h1>
-      <input type='text' name='firstName' id='firstName' />
-      <input
-        type='button'
-        value='Search'
-        onClick={() => getFirstName(document.querySelector("#firstName").value)}
-      />
+      <h1>User Name Search</h1>
+      <div className='form'>
+        <input type='text' name='firstName' id='firstName' ref={textBoxRef} />
+        <input type='button' value='Search' onClick={() => getFirstName()} />
+      </div>
+      {returnData === null ? (
+        <h3>No result.</h3>
+      ) : returnData.length === 0 ? (
+        <h3>Loading...</h3>
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <td>ID</td>
+              <td>First Name</td>
+              <td>Last Name</td>
+              <td>Email</td>
+              <td>Gender</td>
+            </tr>
+          </thead>
+          <tbody>
+            {returnData === []
+              ? null
+              : returnData.map((d, key) => (
+                  <tr key={key}>
+                    <td>{d.id}</td>
+                    <td>{d.first_name}</td>
+                    <td>{d.last_name}</td>
+                    <td>{d.email}</td>
+                    <td>{d.gender}</td>
+                  </tr>
+                ))}
+          </tbody>
+        </table>
+      )}
     </>
   );
 }
